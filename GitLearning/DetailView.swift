ddf875cc046df4f1406fc6ce69b9a5e435066f0d
Eloy Hernandez <eloy@prettysimplemarketing.com>
2025-02-28 20:05:05 -0500

    Added navigation link to ContentView and added DetailView

---
+++ GitLearning/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  GitLearning
+//
+//  Created by Eloy Hernandez on 2/28/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("An App To Learn Git")
+    }
+}
+
+#Preview {
+    DetailView()
+}
