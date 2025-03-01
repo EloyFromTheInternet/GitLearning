99e5435f06ae190a181cac3af66421ab9ff8698c
Eloy Hernandez <eloy@prettysimplemarketing.com>
2025-02-28 20:51:42 -0500

    Changed button text to show details

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                 Button (action: {
                     showSheet = true
                 }, label: {
-                Text("Show sheet to true")
+                Text("Show Details")
                 })
             }
             .padding()
