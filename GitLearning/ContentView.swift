b5d8da7a7fbfc745b3907b829d11e04cc086af0c
Eloy Hernandez <eloy@prettysimplemarketing.com>
2025-02-28 19:46:23 -0500

    Initial Commit

---
+++ GitLearning/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GitLearning
+//
+//  Created by Eloy Hernandez on 2/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Eloy Hernandez on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
